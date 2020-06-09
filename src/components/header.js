f55db298c56f4be54a543336ef74f352b0a8aad0
Luis Fernando Cifuentes <lfcifuentes28@gmail.com>
2020-06-09 07:20:13 -0500

    Add email

--- src/components/header.js
+++ src/components/header.js
@@ -10,6 +10,7 @@ import coding from "../img/coding.svg"
 import github_brand from "../img/github.svg"
 import gitlab_brand from "../img/gitlab.svg"
 import linkedin_brand from "../img/linkedin.svg"
+import envelop from "../img/envelope.svg"
 
 export default () => {
   const name = "Luis Fernando Cifuentes"
@@ -21,20 +22,26 @@ export default () => {
           <p className="font-light text-xl">
             Web Developer, PHP, JS, Golang Lover
           </p>
-          <span className="flex">
-          <a className="mx-2" href="https://dev.to/luisfercho" target="_blank" rel="noopener noreferrer">
-            <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt={name+" DEV Profile"} height="30" width="30" />
-          </a>
-          <a className="mx-2" href="https://github.com/luisfercho" target="_blank" rel="noopener noreferrer">
-            <img src={github_brand} alt={name+" GitHub Profile"} height="30" width="30" />
-          </a>
-          <a className="mx-2" href="https://gitlab.com/luisfercho" target="_blank" rel="noopener noreferrer">
-            <img src={gitlab_brand} alt={name+" GitLab Profile"} height="30" width="30" />
-          </a>
-          <a className="mx-2" href="https://www.linkedin.com/in/luis-fernando-cifuentes-3a6a90119/" target="_blank" rel="noopener noreferrer">
-            <img src={linkedin_brand} alt={name+" LinkedIn Profile"} height="30" width="30" />
-          </a>
-        </span>
+          <span className="flex mt-2">
+            <a className="mr-2" href="https://dev.to/luisfercho" target="_blank" rel="noopener noreferrer">
+              <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt={name+" DEV Profile"} height="30" width="30" />
+            </a>
+            <a className="mx-2" href="https://github.com/luisfercho" target="_blank" rel="noopener noreferrer">
+              <img src={github_brand} alt={name+" GitHub Profile"} height="30" width="30" />
+            </a>
+            <a className="mx-2" href="https://gitlab.com/luisfercho" target="_blank" rel="noopener noreferrer">
+              <img src={gitlab_brand} alt={name+" GitLab Profile"} height="30" width="30" />
+            </a>
+            <a className="mx-2" href="https://www.linkedin.com/in/luis-fernando-cifuentes-3a6a90119/" target="_blank" rel="noopener noreferrer">
+              <img src={linkedin_brand} alt={name+" LinkedIn Profile"} height="30" width="30" />
+            </a>
+          </span>
+          <span className="flex pt-3 items-center">
+            <img src={envelop} alt={name+" LinkedIn Profile"} height="30" width="30" />
+            <span className="pl-2">
+              E-mail: lfcifuentes28@gmail.com
+            </span>
+          </span>
         </div>
         <img src={coding} alt="Developer" className="image-header"/>
       </div>
